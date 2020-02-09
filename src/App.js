e4069b205825af2463d341146cf2e71cd92ec79a
surrajj20 <shah.suraj1200@gmail.com>
2020-02-10 02:06:52 +0530

    user page updated

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    // repos: [],
     loading: false,
     alert: null
   };
@@ -40,6 +41,17 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
 
   }
+
+  // // Get users repos
+  // getUserRepos = async (username) => {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+  //   this.setState({ repos: res.data, loading: false });
+  // }
+
+
+
 
   //Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
