bd0713be045e0688362b87be1ba9f46e9539cdd9
surrajj20 <shah.suraj1200@gmail.com>
2020-02-13 02:16:28 +0530

    reducer & action created

--- src/App.js
+++ src/App.js
@@ -20,22 +20,6 @@ const App = () => {
   const [alert, setAlert] = useState(null);
 
 
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-  //   this.setState({ users: res.data, loading: false });
-  // }
-
-  ///Search Github Users
-  const searchUsers = async text => {
-    setLoading(true);
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setUsers(res.data.items);
-    setLoading(false);
-  }
-
   //Get single Github user
   const getUser = async (username) => {
     setLoading(true);
@@ -82,7 +66,7 @@ const App = () => {
             <Switch>
               <Route exact path='/' render={props => (
                 <Fragment>
-                  <Search searchUsers={searchUsers} clearUsers={clearUsers} showClear={users.length > 0 ? true : false} setAlert={showAlert} />
+                  <Search clearUsers={clearUsers} showClear={users.length > 0 ? true : false} setAlert={showAlert} />
                   <Users loading={loading} users={users} />
 
                 </Fragment>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+const Search = ({ showClear, clearUsers, setAlert }) => {
+    const githubContext = useContext(GithubContext);
+
+
     const [text, setText] = useState('');
 
 
@@ -11,7 +15,7 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
             setAlert('Please enter something', 'light');
 
         } else {
-            searchUsers(text);
+            githubContext.searchUsers(text);
             setText('');
         }
     };
@@ -35,7 +39,6 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
 };
 
 Search.propTypes = {
-    searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
     showClear: PropTypes.bool.isRequired,
     setAlert: PropTypes.func.isRequired
