6844ee943f6b9807b136b86853478cc5b8292bc2
surrajj20 <shah.suraj1200@gmail.com>
2020-02-13 03:23:10 +0530

    ReposState move to context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -43,6 +43,15 @@ const GithubState = props => {
     };
 
     //Get Reops
+    const getUserRepos = async (username) => {
+        setLoading();
+        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+        dispatch({
+            type: GET_REPOS,
+            payload: res.data
+        });
+    };
 
     //clear Users
     const clearUsers = () => dispatch({ type: CLEAR_USERS });
@@ -59,7 +68,8 @@ const GithubState = props => {
             loading: state.loading,
             searchUsers,
             clearUsers,
-            getUser
+            getUser,
+            getUserRepos
         }}
     >
         {props.children}
